0a2f4b4e87fab86ac8b834ab99f49c184851d97d
KYUSEONGHAN <tyty587587@gmail.com>
2022-11-17 16:38:04 +0900

    2022.11.17.목: hks, 실시간 음성인식 및 텍스트 변환 외부 & 내부 설정, python3

--- sound/voice_recognition.py
+++ sound/voice_recognition.py
@@ -9,8 +9,11 @@ p = pyaudio.PyAudio()
 for i in range(p.get_device_count()):
     print(p.get_device_info_by_index(i))
 
-with sr.Microphone(1) as source:
-    r.adjust_for_ambient_noise(source, 1)  # Adjust for ambient
+# num = 0  # 외부 모니터 없이 돌릴 시,
+num = 1  # 외부 모니터 연결되었을 시,
+
+with sr.Microphone(num) as source:
+    r.adjust_for_ambient_noise(source, num)  # Adjust for ambient
     print("Say something!")
     audio = r.listen(source)
 print("Runnnnnn")
